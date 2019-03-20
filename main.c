00e3e96cbc2746916a02e3b535b572f04f7d041b
mailpratapr <mailpratapr@gmail.com>
2019-03-20 02:57:48 -0700

    Update main.c
    Added examples

--- main.c
+++ main.c
@@ -4,11 +4,22 @@
 #include<stdint.h> 
 #include "spi.h"
 
-int main()
-{	
-	SPIInit();
-	eeprom_chip_erase();
-	
+void main() {
+  uint8_t data;
+  uint8_t tx[256] = {
+    0x05
+  };
+  uint8_t rx[256] = {
+    0x00
+  };
+  SPIInit();
+  eeprom_chip_erase(); //Erase chip
+
+  write_byte(0xA0, 0x05); //Write one byte
+  data = read_byte(0xAA); //Read one byte
+
+  write_page(0xA00, tx); //Write one page data
+  read_page(0xA00, rx); //Read one page
+
+  printf("Done");
 }
-
-
